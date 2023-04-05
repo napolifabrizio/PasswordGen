45d0fc0b297592449d0774855184589c5c177617
Fabrizio <fabrizionapoli50@gmail.com>
2023-04-05 10:15:21 -0300

    alteracoes HTML

--- src/js/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-const msgNumber = prompt("Deseja ter números na sua senha?");
-// const msgLetter = prompt("Deseja ter letras na sua senha?");
-// const msgCharac = prompt("Deseja ter caracteres especiais na sua senha?");
-// const msgDigits = prompt("Deseja ter quantos digitos na sua senha?");
