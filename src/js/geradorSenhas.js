695f6edb3c595a3b58de8f1948769c675930bc12
Fabrizio <fabrizionapoli50@gmail.com>
2023-04-18 09:01:51 -0300

    pequena alteracao geradorSenhas

--- src/js/geradorSenhas.js
+++ src/js/geradorSenhas.js
@@ -1,17 +1,51 @@
+import digitosSenha from "./arraysDigitos.js";
+
 function randomNumberInterval(a, b) {
   return Math.floor(Math.random() * (b - a + 1)) + a;
 }
 
-function creatPassword(senhaTamanho, digitosSenha, senhaFinal, contador) {
-  for (let i = 0; i < Number(senhaTamanho) - contador; i++) {
-    senhaFinal.push(
-      digitosSenha[randomNumberInterval(0, digitosSenha.length - 1)]
+function createPassword(primeira, segunda, terceira, tamanhoSenha) {
+  let amount = 0;
+  const all = [];
+  const password = [];
+
+  if (primeira) {
+    all.push(...digitosSenha.numbers);
+    password.push(
+      digitosSenha.numbers[
+        randomNumberInterval(0, digitosSenha.numbers.length - 1)
+      ]
+    );
+    amount++;
+  }
+
+  if (segunda) {
+    all.push(...digitosSenha.letter);
+    password.push(
+      digitosSenha.letter[
+        randomNumberInterval(0, digitosSenha.letter.length - 1)
+      ]
+    );
+    amount++;
+  }
+
+  if (terceira) {
+    all.push(...digitosSenha.charac);
+    password.push(
+      digitosSenha.charac[
+        randomNumberInterval(0, digitosSenha.charac.length - 1)
+      ]
+    );
+    amount++;
+  }
+
+  for (let i = 0; i < Number(tamanhoSenha) - amount; i++) {
+    password.push(
+      all[randomNumberInterval(0, all.length - 1)]
     );
   }
 
-  return senhaFinal.join("");
+  return password.join("");
 }
 
-export {creatPassword, randomNumberInterval};
-
-
+export default createPassword;
